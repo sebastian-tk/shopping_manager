44ae2cf8b59f20d49c2c36826731dcc834afb9a8
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-11-29 14:05:52 +0100

    refactor: Use interfaces in getCustomerWithProduct
    + Added interfaces Function in CustomerWithProductUtils
    + Used interfaces in getCustomerWithProduct instead methods getter

--- persistence/src/main/java/tkaczyk/sebastian/persistence/CustomerWithProductUtils.java
+++ persistence/src/main/java/tkaczyk/sebastian/persistence/CustomerWithProductUtils.java
@@ -5,5 +5,7 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 public interface CustomerWithProductUtils {
-  
+    Function<CustomerWithProduct,Customer> toCustomer =customerWithProduct -> customerWithProduct.customer;
+    Function<CustomerWithProduct, Stream<Product>> toProductsStream = customerWithProduct -> customerWithProduct.products.stream();
+
 }
--- service/src/main/java/tkaczyk/sebastian/service/ShoppingService.java
+++ service/src/main/java/tkaczyk/sebastian/service/ShoppingService.java
@@ -3,7 +3,6 @@ package tkaczyk.sebastian.service;
 import lombok.Getter;
 import tkaczyk.sebastian.exception.ShoppingServiceException;
 import tkaczyk.sebastian.persistence.Customer;
-import tkaczyk.sebastian.persistence.CustomerWithProduct;
 import tkaczyk.sebastian.persistence.Product;
 import tkaczyk.sebastian.persistence.converter.CustomerWithProductJsonConverter;
 
@@ -11,6 +10,8 @@ import java.util.Map;
 
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
+import static tkaczyk.sebastian.persistence.CustomerWithProductUtils.toCustomer;
+import static tkaczyk.sebastian.persistence.CustomerWithProductUtils.toProductsStream;
 
 @Getter
 public class ShoppingService {
@@ -36,14 +37,16 @@ public class ShoppingService {
                 .orElseThrow(()-> new ShoppingServiceException("Cannot read data from json filename: " + fileName))
                 .stream()
                 .collect(groupingBy(
-                        CustomerWithProduct::getCustomer,
+                        toCustomer,
                         collectingAndThen(flatMapping(
-                                customerWithProducts -> customerWithProducts.getProducts().stream(),toList()),
-                                products -> products
-                                        .stream()
-                                        .collect(groupingBy(
-                                                identity(),
-                                                counting()))
+                                        toProductsStream,toList()),
+                                        products -> products
+                                                .stream()
+                                                .collect(groupingBy(
+                                                        identity(),
+                                                        counting()))
                         )));
     }
+
+
 }
