8482543508aadd088825e0d89e9e26e42db58473
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-11-30 17:18:28 +0100

    test: Add FactoryCustomer ShoppingService test
    + Added FactoryCustomer in package extension for test ShoppingService

---
+++ service/src/test/java/tkaczyk/sebastian/service/extensions/FactoryCustomer.java
@@ -0,0 +1,43 @@
+package tkaczyk.sebastian.service.extensions;
+
+import tkaczyk.sebastian.persistence.Customer;
+
+import java.math.BigDecimal;
+
+public interface FactoryCustomer {
+    Customer zygmunt = Customer
+            .builder()
+            .id(1)
+            .name("Zygmunt")
+            .surname("Nowak")
+            .age(65)
+            .cash(BigDecimal.valueOf(30000))
+            .build();
+
+    Customer tomasz = Customer
+            .builder()
+            .id(2)
+            .name("Tomasz")
+            .surname("Jonak")
+            .age(25)
+            .cash(BigDecimal.valueOf(3500))
+            .build();
+
+    Customer iza = Customer
+            .builder()
+            .id(3)
+            .name("Iza")
+            .surname("Wolna")
+            .age(21)
+            .cash(BigDecimal.valueOf(8000))
+            .build();
+
+    Customer marta = Customer
+            .builder()
+            .id(4)
+            .name("Marta")
+            .surname("Białek")
+            .age(17)
+            .cash(BigDecimal.valueOf(900))
+            .build();
+}
