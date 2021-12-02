3909e3aa6e32bf80f61e970a9c9c11ae579e2139
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-12-02 12:49:53 +0100

    test: Add FactoryProduct for tests of ShoppingService

---
+++ service/src/test/java/tkaczyk/sebastian/service/extensions/FactoryProduct.java
@@ -0,0 +1,31 @@
+package tkaczyk.sebastian.service.extensions;
+
+import tkaczyk.sebastian.persistence.Product;
+
+import java.math.BigDecimal;
+
+public interface FactoryProduct {
+    Product productId8 = Product
+            .builder()
+            .id(8)
+            .name("Telewizor")
+            .category("Elektronika")
+            .price(BigDecimal.valueOf(6400))
+            .build();
+
+    Product productId5 = Product
+            .builder()
+            .id(5)
+            .name("Ogniem i mieczem")
+            .category("Ksiazka")
+            .price(BigDecimal.valueOf(78))
+            .build();
+
+    Product productId6 = Product
+            .builder()
+            .id(6)
+            .name("Sok pomaranczowy")
+            .category("Spozywcze")
+            .price(BigDecimal.valueOf(5))
+            .build();
+}
